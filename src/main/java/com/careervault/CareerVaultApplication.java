22d09b61128e27337097d2fc11b306ef435d5239
Sava Sathwik <savasathwik2@gmail.com>
2026-09-07 21:41:48 +0530

    feat: initialize Spring Boot application

---
+++ src/main/java/com/careervault/CareerVaultApplication.java
@@ -0,0 +1,12 @@
+package com.careervault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerVaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CareerVaultApplication.class, args);
+    }
+}
---
+++ src/test/java/com/careervault/CareervaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careervault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareervaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
